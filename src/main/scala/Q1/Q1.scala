a818f15b7beeb75b8653e0ad15e4da4c5e96aa39
Yashed <yashedthisara2001@gmail.com>
2023-08-09 10:53:25 +0530

    Add q1

--- src/main/scala/Q1/Q1.scala
+++ src/main/scala/Q1/Q1.scala
@@ -2,4 +2,13 @@ package Q1
 
 object Q1 {
 
+  def filterEvenNumbers(li:List[Int]): List[Int] = {
+    li.filter(li => li%2 ==0)
+  }
+  def main(args:Array[String]): Unit={
+    val num = List(1,2,3,4,5,6,7,8,9,10)
+    val evenNum = filterEvenNumbers(num)
+    println("Even numbers = " + evenNum)
+  }
+
 }
